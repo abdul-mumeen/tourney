f0ab3ccc02663b251d78357f86193f8c3c309ea3
Abdulmalik-Olasode <olasodemalik@gmail.com>
2025-08-10 17:30:21 +0100

    Added a signin/signout button

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+console.log(process.env.GOOGLE_CLIENT_ID ,"term")
+
+export const authOptions = {
+  // debug:true,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useSession } from "next-auth/react";
+import { handleSignIn, handleSignOut } from "@/lib/auth";
 
 export default function Home() {
+  const { data: session } = useSession()
+  console.log(process.env.NEXTAUTH_URL,"aux", process.env.GOOGLE_CLIENT_ID ,"term" )
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -21,21 +28,48 @@ export default function Home() {
         </ol>
 
         <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
+          {!session ? (
+            <button
+             style={ {
+               padding: "15px",
+              border: "1px solid #ccc",
+              borderRadius: "8px",
+              textAlign: "center",
+              maxWidth: "300px",
+              backgroundColor:"white",
+              color:"black"
+             }}
+              className={styles.primary}
+              onClick={handleSignIn}
+            >
+              
+              {/* <Image
+                className={styles.logo}
+                src="/vercel.svg"
+                alt="Vercel logomark"
+                width={20}
+                height={20}
+              /> */}
+              Sign in with Google
+            </button>
+          ) : (
+            <button
+              className={styles.primary}
+              onClick={handleSignOut}
+                style={ {
+               padding: "15px",
+              border: "1px solid #ccc",
+              borderRadius: "8px",
+              textAlign: "center",
+              maxWidth: "300px",
+              backgroundColor:"white",
+              color:"black"
+             }}
+            >
+              Sign out
+            </button>
+          )}
+
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
@@ -45,7 +79,36 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+
+        {session && (
+          <div
+            style={{
+              marginTop: "20px",
+              padding: "15px",
+              border: "1px solid #ccc",
+              borderRadius: "8px",
+              textAlign: "center",
+              maxWidth: "300px",
+              backgroundColor:"white",
+               color:"blue",
+            }}
+          >
+            <Image
+              src={session.user?.image || "/default-avatar.png"}
+              alt="User Avatar"
+              width={80}
+              height={80}
+              style={{ borderRadius: "50%",
+                      
+                       
+               }}
+            />
+            <h3>{session.user?.name}</h3>
+            <p>{session.user?.email}</p>
+          </div>
+        )}
       </main>
+
       <footer className={styles.footer}>
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
